d268255d9173b5317d1bce8a3b33914011d89f16
David Grove Jørgensen <dgj@kb.dk>
2016-07-15 15:15:26 +0200

    added new document presenter for better titles of letters

--- app/models/letter_document_presenter.rb
+++ app/models/letter_document_presenter.rb
@@ -1,6 +1,6 @@
 class  LetterDocumentPresenter < Blacklight::DocumentPresenter
   def render_document_index_label(field, opts={})
-    type  = @document.first(:cat_ssi)
+    type  = @document.first(:cat_ssi).to_s
     title = 'Ingen titel'
     if (type == 'letter')
       title = "Brev fra "+@document.first(:sender_ssim).to_s + " til " + @document.first(:recipient_ssim).to_s
