b2c6fd9bbff21b32c364f124318fc0de7dd74959
Polyxeni Makri <poma@kb.dk>
2016-07-26 13:39:28 +0200

    Make a helper method for the volume_id_ssi, to show the title of the volume

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,19 @@ module ApplicationHelper
     I18n.t("models.#{name}")
   end
 
+  # Helper method to show the volume title at the letter index
+  def show_volume_link args
+    # Find the id of the volume in the letter's metadata
+    id = args[:document]['volume_id_ssi']
+    return unless id.present?
+    # MAke a direct search request to Solr with the volume id
+    volume_url = Net::HTTP.get(URI('http://bifrost-test-01.kb.dk:8080/solr/blacklight-core/select?q='+URI.escape(id)+'&wt=json&indent=true'))
+    # Parse the response as JSON
+    volume_record_json = JSON.parse(volume_url)
+    # Find the volume title in the JSON
+    title = volume_record_json['response']['docs'].first['volume_title_ssim'].first
+    # Make a link with the volume title as a label that redirects you to the volume landing page
+    link_to title, solr_document_path(id)
+  end
+
 end
