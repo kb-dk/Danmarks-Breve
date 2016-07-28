3e9cc31a29771b783630c11a1a809c07f36b18d3
Polyxeni Makri <poma@kb.dk>
2016-07-28 11:18:32 +0200

    Remove some comments to make it readable

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -76,14 +76,8 @@ class CatalogController < ApplicationController
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
     config.add_facet_field 'cat_ssi', :label => I18n.t('blacklight.search.categori'), helper_method: :translate_model_names
-    # config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
-    # config.add_facet_field 'language_facet', label: 'Language', limit: true
-    # config.add_facet_field 'lc_1letter_facet', label: 'Call Number'
-    # config.add_facet_field 'subject_geo_facet', label: 'Region'
-    # config.add_facet_field 'subject_era_facet', label: 'Era'
 
     # config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['cat_ssi', 'language_facet']
-
     # config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
     #    :years_5 => { label: 'within 5 Years', fq: "published_date_ssi:[#{Time.zone.now.year - 5 } TO *]" },
     #    :years_10 => { label: 'within 10 Years', fq: "published_date_ssi:[#{Time.zone.now.year - 10 } TO *]" },
@@ -98,16 +92,6 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    # config.add_index_field 'title_display', label: 'Title'
-    # config.add_index_field 'title_vern_display', label: 'Title'
-    # config.add_index_field 'author_display', label: 'Author'
-    # config.add_index_field 'author_vern_display', label: 'Author'
-    # config.add_index_field 'cat_ssi', label: 'Format'
-    # config.add_index_field 'language_facet', label: 'Language'
-    # config.add_index_field 'published_display', label: 'Published'
-    # config.add_index_field 'published_vern_display', label: 'Published'
-    # config.add_index_field 'lc_callnum_display', label: 'Call number'
-
     config.add_index_field 'author_ssi', :label => 'Forfatter'
     config.add_index_field 'publisher_ssi', :label => 'Udgivelsesoplysninger'
     config.add_index_field 'published_place_ssi', :label => 'Udgivelsessted'
@@ -121,22 +105,8 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    # config.add_show_field 'title_display', label: 'Title'
-    # config.add_show_field 'title_vern_display', label: 'Title'
-    # config.add_show_field 'subtitle_display', label: 'Subtitle'
-    # config.add_show_field 'subtitle_vern_display', label: 'Subtitle'
-    # config.add_show_field 'author_display', label: 'Author'
-    # config.add_show_field 'author_vern_display', label: 'Author'
-    # config.add_show_field 'cat_ssi', label: 'Format'
-    # config.add_show_field 'url_fulltext_display', label: 'URL'
-    # config.add_show_field 'url_suppl_display', label: 'More Information'
-    # config.add_show_field 'language_facet', label: 'Language'
-    # config.add_show_field 'published_display', label: 'Published'
-    # config.add_show_field 'published_vern_display', label: 'Published'
-    # config.add_show_field 'lc_callnum_display', label: 'Call number'
-    # config.add_show_field 'isbn_t', label: 'ISBN'
 
-    # Add more fields later
+    # Letter specific metadata
     config.add_show_field 'sender_tesim', :label => I18n.t('blacklight.search.sender')
     config.add_show_field 'recipient_tesim', :label => I18n.t('blacklight.search.recipient')
     config.add_show_field 'sender_location_tesim', :label =>  I18n.t('blacklight.search.senders_location')
@@ -228,9 +198,6 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # # except in the relevancy case).
     config.add_sort_field 'score desc', label: 'relevance'
-    # config.add_sort_field 'pub_date_sort desc, title_sort asc', label: 'year'
-    # config.add_sort_field 'author_sort asc, title_sort asc', label: 'author'
-    # config.add_sort_field 'title_sort asc, pub_date_sort desc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
