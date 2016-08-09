5cda89c42bb7f999b43289a9e4a7a7083df77f38
Polyxeni Makri <poma@kb.dk>
2016-08-09 15:23:41 +0200

    Revert the routes, to recognize the format

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,14 @@ Rails.application.routes.draw do
   root to: "catalog#index"
     concern :searchable, Blacklight::Routes::Searchable.new
 
-  # Rails does not accept dots in the ids, as it appends :format in every rule (.xml, .html etc)
-  # So in constraints, we add this regex for the id (that doesn't allow * in the id), so we
-  # can override the default Rails functionality.
-  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do#, requirements: {:id =>  (/.+?(.xml)/ if :id.to_s.include? "xml") } do
-  # resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog', constraints: {:id => /[^\*]+/} do
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
     concerns :searchable
   end
 
   devise_for :users
   concern :exportable, Blacklight::Routes::Exportable.new
 
-  # Rails does not accept dots in the ids, as it appends :format in every rule (.xml, .html etc)
-  # So in constraints, we add this regex for the id (that doesn't allow * in the id), so we
-  # can override the default Rails functionality.
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do#, requirements:  {:id => (/.+?(.xml)/ if :id.to_s.include? "xml") } do
-  # resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog', constraints: {:id => /[^\*]+/} do
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :exportable
   end
 
@@ -32,10 +24,6 @@ Rails.application.routes.draw do
       delete 'clear'
     end
   end
-
- # Rails.application.routes.draw do
- #   get "/pages/:page" => "pages#show"
- # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
