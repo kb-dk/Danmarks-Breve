774c046562c38250f4210d6ff2551edb66a7695a
Polyxeni Makri <poma@kb.dk>
2016-08-12 12:17:02 +0200

    Change the default citation

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -16,4 +16,17 @@ class SolrDocument
   # and Blacklight::Document::SemanticFields#to_semantic_values
   # Recommendation: Use field names from Dublin Corele
   use_extension(Blacklight::Document::DublinCore)
+
+  def export_as_apa_citation_txt
+    doc = self.to_hash
+    cite = ""
+    volume = Finder.get_doc_by_id(doc['volume_id_ssi']).first
+    cite +=  volume['author_name_tesim'].to_sentence(:last_word_connector => ' og ') + ", " unless volume['author_name_tesim'].blank?
+    cite +=  volume['published_date_ssi'] + ", " unless volume['published_date_ssi'].blank?
+    cite +=  "<i>"+volume['volume_title_tesim'].first + "</i>, " unless volume['volume_title_tesim'].blank?
+    cite +=  "side "+doc['page_ssi'] + ", " unless doc['page_ssi'].blank?
+    cite +=  volume['published_place_ssi'] + ": " unless volume['published_place_ssi'].blank?
+    cite +=  volume['publisher_name_ssi'] + " " unless volume['publisher_name_ssi'].blank?
+    cite.html_safe
+  end
 end
