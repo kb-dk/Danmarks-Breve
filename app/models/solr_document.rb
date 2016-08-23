4d26c31721a795f9e8ee5a9d30ae7036b6c5b578
Sigfrid Lundberg <siggelundberg@gmail.com>
2016-08-23 13:23:48 +0200

    working on citation

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -19,14 +19,28 @@ class SolrDocument
 
   def export_as_apa_citation_txt
     doc = self.to_hash
-    cite = ""
+
+    recipient = doc['recipient_tesim'].to_sentence(:last_word_connector => ' og ')
+    sender = doc['sender_tesim'].to_sentence(:last_word_connector => ' og ')
+    date = doc['date_ssim'].to_sentence()
+    title = " BREV "
+    recipient.first.present? ? title += "TIL: " + recipient.to_s : title = 'TIL: ukendt'
+    sender.first.present? ? title += " FRA: " + sender.to_s : title += ' FRA: ukendt'
+    date.first.present? ? title += " (" + date.to_s + ")" : title += ' (dato ukendt)'
+
     volume = Finder.get_doc_by_id(doc['volume_id_ssi']).first
-    cite +=  volume['author_name_tesim'].to_sentence(:last_word_connector => ' og ') + ", " unless volume['author_name_tesim'].blank?
-    cite +=  volume['published_date_ssi'] + ", " unless volume['published_date_ssi'].blank?
-    cite +=  "<i>"+volume['volume_title_tesim'].first + "</i>, " unless volume['volume_title_tesim'].blank?
+    auth = volume['author_name_tesim'].to_sentence(:last_word_connector => ' og ').to_s
+
+    cite = ""
+    cite +=  auth  + ", " unless auth.blank?
+    cite +=  volume['published_date_ssi'] + ". " unless volume['published_date_ssi'].blank?
+    cite +=  title
+    cite +=  " i: <i>"+volume['volume_title_tesim'].first + "</i>, " unless volume['volume_title_tesim'].blank?
     cite +=  "side "+doc['page_ssi'] + ", " unless doc['page_ssi'].blank?
     cite +=  volume['published_place_ssi'] + ": " unless volume['published_place_ssi'].blank?
     cite +=  volume['publisher_name_ssi'] + " " unless volume['publisher_name_ssi'].blank?
+
     cite.html_safe
+
   end
 end
