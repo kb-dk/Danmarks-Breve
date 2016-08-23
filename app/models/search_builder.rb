92982c7d68dde7913b5e72f01310cbd54347130b
David Grove Jørgensen <dgj@kb.dk>
2016-08-23 15:22:43 +0200

    trying to solve another url decode problem

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -5,4 +5,16 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
   self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
+
+  def where conditions
+    if conditions[:id].present?
+      if conditions[:id].is_a? Array
+        conditions[:id].map!{|s| URI.decode(s)}
+      end
+      if conditions[:id].is_a? String
+        conditions[:id] = URI.decode(conditions[:id])
+      end
+    end
+    super conditions
+  end
 end
