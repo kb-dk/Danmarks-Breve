7e3b6912324be0894ba9b2d2792f8a60950c7d80
xenma <poma@kb.dk>
2016-12-19 12:32:15 +0100

    Add sender and recipient location in facets

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -79,6 +79,8 @@ class CatalogController < ApplicationController
     config.add_facet_field 'cat_ssi', :label => I18n.t('blacklight.search.categori'), helper_method: :translate_model_names
     config.add_facet_field 'sender_ssim', :label => "Afsender", :limit => 20
     config.add_facet_field 'recipient_ssim', :label => "Modtager", :limit => 20
+    config.add_facet_field 'sender_location_ssim', :label => I18n.t('blacklight.search.facets.sender_location'), :limit => 20
+    config.add_facet_field 'recipient_location_ssim', :label => I18n.t('blacklight.search.facets.recipient_location'), :limit => 20
     config.add_facet_field 'year_itsi', label: 'Afsendelsesdato',
                            range: {
                                num_segments: 10,
